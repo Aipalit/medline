604ff0d8a4c648306082b04fc95c487ef24d86d5
Aipalit <lucas.gomes.souza1997@gmail.com>
2025-05-05 22:25:22 -0300

    feat:Implementacao entidade Paciente,TipoAtendimento e FilaDeEspera

---
+++ backend/src/main/java/com/medline/application/FilaDeEspera.java
@@ -0,0 +1,47 @@
+package com.medline.application;
+
+import java.time.LocalDateTime;
+
+import com.medline.application.model.Paciente;
+import com.medline.application.model.TipoAtendimento;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "filas_de_espera")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FilaDeEspera {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "paciente_id", nullable = false)
+    private Paciente paciente;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_atendimento_id", nullable = false)
+    private TipoAtendimento tipoAtendimento;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime dataEntrada;
+
+    @Column(length = 20, columnDefinition = "VARCHAR(20) DEFAULT 'AGUARDANDO'")
+    private String status;
+
+    private LocalDateTime dataInicioAtendimento;
+    private LocalDateTime dataFimAtendimento;
+}
---
+++ backend/src/main/java/com/medline/application/model/TipoAtendimento.java
@@ -0,0 +1,34 @@
+package com.medline.application.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tipos_atendimento")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TipoAtendimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(length = 255)
+    private String medicoResponsavel;
+
+}
